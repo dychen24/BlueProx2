33d3b5d8fd7c4ea569d159252cd5a9cfab480d76
dychen <luckychendy@gmail.com>
2016-07-22 13:43:01 -0700

    android project of BlueProx

---
+++ app/src/main/java/com/chendy/blueprox/bluescan.java
@@ -0,0 +1,15 @@
+package com.chendy.blueprox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.location.LocationManager;
+
+
+public class bluescan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluescan);
+    }
+}
